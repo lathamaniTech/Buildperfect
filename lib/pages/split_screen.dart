3c7c4d27b141bfae988a71d71f662b0f10ea4cf8
KarthickTechie <reachkarthickprogramer@gmail.com>
2025-09-23 23:33:33 +0530

    initial branch creation

---
+++ lib/pages/split_screen.dart
@@ -0,0 +1,17 @@
+/*
+@author     : karthick.d  23/09/2025
+@desc       : split screen divides the scree in to two section where one part 
+              is for widgets for draggable  and another for dragtarget canvas
+              
+
+*/
+
+import 'package:flutter/widgets.dart';
+
+class SplitScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
