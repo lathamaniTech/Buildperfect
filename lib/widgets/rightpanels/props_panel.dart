80c517e5368c20a5a0b6cf3e28d826f5d1b36da2
KarthickTechie <reachkarthickprogramer@gmail.com>
2025-10-06 14:56:37 +0530

    init : keyvaluetextbox custom control created

--- lib/widgets/rightpanels/props_panel.dart
+++ lib/widgets/rightpanels/props_panel.dart
@@ -1,4 +1,5 @@
 import 'package:dashboard/bloc/bpwidgetprops/bpwidget_props_bloc.dart';
+import 'package:dashboard/widgets/customcontrols/key_value_textbox.dart';
 import 'package:dashboard/widgets/rightpanels/panel_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,237 +33,45 @@ class _PropsPanelState extends State<PropsPanel> {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 80,
-                          child: Text(
-                            'Label Text',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: widget.width * 0.5,
-                        height: 25,
-
-                        child: TextField(
-                          style: TextStyle(fontSize: 12),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: KeyValueTextbox(
+                    labeltext: 'Label Text',
+                    width: widget.width,
                   ),
-
-                  // Row(
-                  //   children: [
-                  //     Text('Label'),
-                  //   ],
-                  // ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 80,
-                          child: Text(
-                            'Control Name',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: widget.width * 0.5,
-                        height: 25,
-
-                        child: TextField(
-                          style: TextStyle(fontSize: 12),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: KeyValueTextbox(
+                    labeltext: 'Control Name',
+                    width: widget.width,
                   ),
-
-                  // Row(
-                  //   children: [
-                  //     Text('Label'),
-                  //   ],
-                  // ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 80,
-                          child: Text(
-                            'Control type',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: widget.width * 0.5,
-                        height: 25,
-
-                        child: TextField(
-                          style: TextStyle(fontSize: 12),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: KeyValueTextbox(
+                    width: widget.width,
+                    labeltext: 'Control Type',
                   ),
-
-                  // Row(
-                  //   children: [
-                  //     Text('Label'),
-                  //   ],
-                  // ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 80,
-                          child: Text(
-                            'Required ?',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: widget.width * 0.5,
-                        height: 25,
-
-                        child: TextField(
-                          style: TextStyle(fontSize: 12),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: KeyValueTextbox(
+                    width: widget.width,
+                    labeltext: 'Required ?',
                   ),
-
-                  // Row(
-                  //   children: [
-                  //     Text('Label'),
-                  //   ],
-                  // ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 80,
-                          child: Text(
-                            'Verification Required ?',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: widget.width * 0.5,
-                        height: 25,
-
-                        child: TextField(
-                          style: TextStyle(fontSize: 12),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: KeyValueTextbox(
+                    width: widget.width,
+                    labeltext: 'Verification Required?',
                   ),
-
-                  // Row(
-                  //   children: [
-                  //     Text('Label'),
-                  //   ],
-                  // ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: SizedBox(
-                          width: 80,
-                          child: Text(
-                            'Validations',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 1.2,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: widget.width * 0.5,
-                        height: 25,
-
-                        child: TextField(
-                          style: TextStyle(fontSize: 12),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: KeyValueTextbox(
+                    width: widget.width,
+                    labeltext: 'Validations',
                   ),
-
-                  // Row(
-                  //   children: [
-                  //     Text('Label'),
-                  //   ],
-                  // ),
                 ),
               ],
             ),
